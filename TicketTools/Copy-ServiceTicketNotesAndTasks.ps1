a9e1bcab20a4f0fd20b907de4efe5dea31968720
Terry Wilson <twilson@greenloopsolutions.com>
2022-05-03 09:47:07 -0700

    refactor: prompt for client ID

--- TicketTools/Copy-ServiceTicketNotesAndTasks.ps1
+++ TicketTools/Copy-ServiceTicketNotesAndTasks.ps1
@@ -7,14 +7,23 @@ $connectWiseFQDN = Read-Host "Provide your ConnectWise Manage Server FQDN"
 $connectWiseCompany = Read-Host "Provide your ConnectWise Company ID"
 $publicKey = Read-Host "API Public Key"
 $privateKey = Read-Host "API Private Key" -AsSecureString
-$sourceTicket = Read-Host "Provide the ticket ID of a ticket to serve as a template. This assumes the variable '$tickets' will already be populated with target tickets."
-#$targetTicket = Read-Host "Target Ticket ID"
+$sourceTicket = Read-Host "Provide the ticket ID of a ticket to serve as a template"
+
+# Confirm the $clientID is a valid GUID format (36 characters including hyphens)
+do {
+    $clientID = Read-Host "Provide your ConnectWise Manage Client ID. Go to https://developer.connectwise.com/ if you need to acquire one."
+    $validGuid = [guid]::TryParse($clientID, $([ref][guid]::Empty))
+    if (-not $validGuid) {
+        Write-Warning "Not a valid GUID format. Try again."
+    }
+} until ($validGuid)
+
 $connectionParams = @{
-Server = "$($connectWiseFQDN)/v4_6_release/apis/3.0"
-ClientID = '2628d75d-c2ea-43c4-bac4-33ea7181d0d6'
-Company = $connectWiseCompany
-PubKey = $publicKey
-PrivateKey = [Marshal]::PtrToStringAuto([Marshal]::SecureStringToBSTR($privateKey))
+    Server     = "$($connectWiseFQDN)/v4_6_release/apis/3.0"
+    ClientID   = $clientID
+    Company    = $connectWiseCompany
+    PubKey     = $publicKey
+    PrivateKey = [Marshal]::PtrToStringAuto([Marshal]::SecureStringToBSTR($privateKey))
 }
 Connect-CWM @connectionParams
 
@@ -26,11 +35,11 @@ foreach ($ticket in $tickets) {
     foreach ($task in $taskNotes) {
         New-CWMTicketTask -ticketId $($ticket.id) -notes $task
     }
-    
+
     foreach ($note in $ticketnotes) {
-       new-cwmticketnote -parentId $($ticket.id) -text $($note.text) -detailDescriptionFlag $($note.detailDescriptionFlag) -internalAnalysisFlag $($note.internalAnalysisFlag) -resolutionFlag $($note.resolutionFlag) -dateCreated $($note.dateCreated) -createdBy $($note.createdBy)
+        New-CWMTicketNote -parentId $($ticket.id) -text $($note.text) -detailDescriptionFlag $($note.detailDescriptionFlag) -internalAnalysisFlag $($note.internalAnalysisFlag) -resolutionFlag $($note.resolutionFlag) -dateCreated $($note.dateCreated) -createdBy $($note.createdBy)
     }
-   
+
 }
 # Disconnect (optional)
 #Disconnect-CWM
